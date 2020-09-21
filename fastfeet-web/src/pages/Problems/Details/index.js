aead556ee6badce7df7b2569d11962215fd0da71
Joao Victor <jvsfernandes924@gmail.com>
2020-09-21 14:25:34 -0300

    Visualizaçao do problema

--- fastfeet-web/src/pages/Problems/Details/index.js
+++ fastfeet-web/src/pages/Problems/Details/index.js
@@ -4,36 +4,14 @@ import PropTypes from 'prop-types';
 import { MdClose } from 'react-icons/md';
 import { WrapperView, Container, Content } from './styles';
 
-export default function Details({ setView }) {
+export default function Details({ setView, problem }) {
   return (
     <WrapperView>
       <Container>
         <MdClose size={20} onClick={() => setView()} />
         <Content>
           <strong>Visualizar Problema</strong>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            asperiores accusantium laboriosam recusandae molestiae! Magnam
-            ducimus quo nihil cumque sequi quia provident aliquam non Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            asperiores accusantium laboriosam recusandae molestiae! Magnam
-            ducimus quo nihil cumque sequi quia provident aliquam non Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Dignissimos Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            asperiores accusantium laboriosam recusandae molestiae! Magnam
-            ducimus quo nihil cumque sequi quia provident aliquam non Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            asperiores accusantium laboriosam recusandae molestiae! Magnam
-            ducimus quo nihil cumque sequi quia provident aliquam non Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. DignissimosLorem
-            ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            asperiores accusantium laboriosam recusandae molestiae! Magnam
-            ducimus quo nihil cumque sequi quia provident aliquam non Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-            asperiores accusantium laboriosam recusandae molestiae! Magnam
-            ducimus quo nihil cumque sequi quia provident aliquam non Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          </p>
+          <p>{problem.description}</p>
         </Content>
       </Container>
     </WrapperView>
@@ -42,4 +20,5 @@ export default function Details({ setView }) {
 
 Details.propTypes = {
   setView: PropTypes.func.isRequired,
+  problem: PropTypes.object.isRequired,
 };
